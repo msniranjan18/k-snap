c1201f46ca771ccf30608ed89796b2963e77ca77
Manvendra S. Niranjan <manvendraniranjan528@gmail.com>
2026-02-11 19:43:44 +0530

    Update author name and pyyaml version in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="k-snap",
     version="1.0.0",
     description="A space-efficient K8s object history recorder and browser using Delta snapshots.",
-    author="Your Name",
+    author="Manvendra",
     py_modules=["ksnap"],
     install_requires=[
-        "pyyaml",
+        "pyyaml>=6.0",
     ],
     entry_points={
         "console_scripts": [
